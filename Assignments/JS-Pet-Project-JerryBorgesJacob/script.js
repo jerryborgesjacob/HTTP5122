bf97bbf5eeb5688e94bd1d4c444856eb52629b36
Jerry Borges <jerryborgesj@gmail.com>
2024-04-19 18:11:17 -0400

    pet project completed

--- Assignments/JS-Pet-Project-JerryBorgesJacob/script.js
+++ Assignments/JS-Pet-Project-JerryBorgesJacob/script.js
@@ -1,5 +1,9 @@
+//use onload to wait for the webpage to load
 window.onload = function(){
 
+    //------------------- ADDING PLAYERS SECTION ---------------------------
+
+    //creating an object to collect Player attributes
     var Player = {
         name: "",
         age: "",
@@ -11,23 +15,31 @@ window.onload = function(){
         defence:"",
         physical:"",
     };
-    
 
+    //creating variables to get elements from HTML
+    var compareContainer = document.getElementById("compare-container");
+    var betterPlayer = document.getElementById("better-player");
+    var error = document.getElementById("data-error");
+    var errorMsg = document.getElementById("error-text");
+    var restart = document.getElementById("restart");
     
+    //creating a list to collect the my team's players
     var playerList = [];
 
-    var button1 = document.getElementById("team1submit");
+    var button1 = document.getElementById("team1submit"); // variable to get MyTeam 
 
-    button1.onclick = AddPlayer;
-    
+    button1.onclick = AddPlayer; //on clicking the button, it leads to AddPLayer function  
 
+    //function to add player to my team
     function AddPlayer(){
 
-        
+        //maximum of 11 players in a team
         if (playerList.length === 11){
-            document.getElementById("form1").innerHTML = "Team is FULL!!";
+            document.getElementById("form1").innerHTML = "Team is FULL!!"; //display message to stop user from adding more players.
+            document.getElementById("form1").style.fontSize = "50px";// message font size
         }
 
+        //getting the values entered in the form elements and sending it to the object's properties
         Player.name = document.getElementById("name").value;
         Player.age = document.getElementById("age").value;
         Player.position = document.getElementById("position").value;
@@ -38,11 +50,11 @@ window.onload = function(){
         Player.defence = document.getElementById("defence").value;
         Player.physical = document.getElementById("physical").value;
 
-        playerList.push({...Player});
-        document.getElementById("team1").reset();
+        playerList.push({...Player});// pushing the data into the array
+        document.getElementById("team1").reset();// reset the form after submit
 
-        console.log(playerList);
-        displayMyTeam();
+        console.log(playerList);//testing the array
+        displayMyTeam();//function to display the player data in a table
 
     }
 
@@ -52,33 +64,37 @@ window.onload = function(){
         var table1 = document.getElementById("table1");
         
         table1.innerHTML = "";
+        //going through the array and adding data to the table
         for (var i = 0; i < playerList.length ; i++){
 
-            var abc = document.createElement("tr");
-            abc.innerHTML = `<td><input type = "radio" id="compare" name="compare" value="${i}"></td><td> ${playerList[i].name} </td><td> ${playerList[i].age} </td><td> ${playerList[i].position} </td>
+            var myTeamRow = document.createElement("tr");//create HTML element to create a new table row
+            myTeamRow.innerHTML = `<td><input type = "radio" id="compare" name="compare" value="${i}"></td><td> ${playerList[i].name} </td><td> ${playerList[i].age} </td><td> ${playerList[i].position} </td>
             <td> ${playerList[i].pace} </td> <td> ${playerList[i].shooting} </td> <td> ${playerList[i].passing} </td> 
             <td> ${playerList[i].dribbling} </td> <td> ${playerList[i].defence} </td> <td> ${playerList[i].physical} </td>`;
 
-            table1.appendChild(abc);
+            table1.appendChild(myTeamRow);//adding the Element to the HTML document
         }
 
 
     }
 
-    var opponentList = [];
+    //creating array to store wishlist data
+    var opponentList = []; //named it opponent instead of wishlist as 'wishlistList' creates confusion as it has the word 'list' 2 times.
 
-    var button2 = document.getElementById("team2submit");
+    var button2 = document.getElementById("team2submit");//getting button id for wishlist form button
 
-    button2.onclick = AddOpponent;
+    button2.onclick = AddOpponent; // on clicking button it leads to function to add opponent data in object
     
-
+    //function to add opponent data in object and then to the table.
     function AddOpponent(){
 
-        
+        //error message to stop adding players after size of 11 (11 players). 
         if (opponentList.length === 11){
             document.getElementById("form2").innerHTML = "Team is FULL!!";
+            document.getElementById("form2").style.fontSize = "50px";
         }
 
+        //getting player attributes from the form and adding it to the object
         Player.name = document.getElementById("name2").value;
         Player.age = document.getElementById("age2").value;
         Player.position = document.getElementById("position2").value;
@@ -89,44 +105,48 @@ window.onload = function(){
         Player.defence = document.getElementById("defence2").value;
         Player.physical = document.getElementById("physical2").value;
 
-        opponentList.push({...Player});
-        document.getElementById("team2").reset();
+        opponentList.push({...Player});// pushing the object data into array
+        document.getElementById("team2").reset();//reset form fields after clicking submit
 
-        console.log(opponentList);
-        displayOpponentTeam();
+        console.log(opponentList);//testing the array
+        displayOpponentTeam();//function to add player data in the table
 
     }
 
     
-
+    //function to add player data in the table
     function displayOpponentTeam(){
         var table2 = document.getElementById("table2");
         
         table2.innerHTML = "";
+        //going through array and displaying the data onto the table
         for (var i = 0; i < opponentList.length ; i++){
 
-            var xyz = document.createElement("tr");
-            xyz.innerHTML = `<td><input type = "radio" id="compare2" name="compare2" value="${i}"></td><td> ${opponentList[i].name} </td><td> ${opponentList[i].age} </td><td> ${opponentList[i].position} </td>
+            var oppRow = document.createElement("tr");//creating HTML element to create a table row to display the player data
+            oppRow.innerHTML = `<td><input type = "radio" id="compare2" name="compare2" value="${i}"></td><td> ${opponentList[i].name} </td><td> ${opponentList[i].age} </td><td> ${opponentList[i].position} </td>
             <td> ${opponentList[i].pace} </td> <td> ${opponentList[i].shooting} </td> <td> ${opponentList[i].passing} </td> 
             <td> ${opponentList[i].dribbling} </td> <td> ${opponentList[i].defence} </td> <td> ${opponentList[i].physical} </td>`;
 
-            table2.appendChild(xyz);
+            table2.appendChild(oppRow);//adding the Element to the HTML document
         }
 
 
     }
 
+    //------------------- COMPARISON SECTION ---------------------------
     var comparison = document.getElementById("comparison");
 
-    comparison.onclick = playerCompareDisplay;
+    comparison.onclick = playerCompareDisplay; //on clicking COMPARE button, it leads to function to compare 2 players
 
+    //function to compare 2 players
     function playerCompareDisplay(){
 
-
+        //getting radio button selected by user of myTeam
         var radio1 = document.querySelector('input[name="compare"]:checked').value;
 
         console.log(radio1);
 
+        //calculating the Overall Number (average of all attributes) of my player
         var myPlayerOverall = Math.ceil((parseInt(playerList[radio1].pace) 
         + parseInt(playerList[radio1].shooting) 
         + parseInt(playerList[radio1].passing) 
@@ -136,6 +156,7 @@ window.onload = function(){
 
         console.log("my player overall = " + myPlayerOverall);
 
+        //displaying the atrributes in compare section
         var selected1 = document.getElementById("player1");
         selected1.innerHTML = `<p>${playerList[radio1].name} </p>
         <p>${playerList[radio1].age} </p>
@@ -149,11 +170,12 @@ window.onload = function(){
         <p id="playerOverall">${myPlayerOverall}</p>`;
 
 
-
+        //getting radio button selected by user of wishlist
         var radio2 = document.querySelector('input[name="compare2"]:checked').value;
 
         console.log(radio2);
 
+        //calculating the Overall Number (average of all attributes) of wishlist player
         var wishlistOverall = Math.ceil((parseInt(opponentList[radio2].pace) 
         + parseInt(opponentList[radio2].shooting) 
         + parseInt(opponentList[radio2].passing) 
@@ -163,8 +185,7 @@ window.onload = function(){
 
         console.log("Wishlist overall = " + wishlistOverall);
 
-
-
+        //displaying the atrributes in compare section
         var selected2 = document.getElementById("player2");
         selected2.innerHTML = `<p>${opponentList[radio2].name} </p> 
         <p>${opponentList[radio2].age} </p> 
@@ -177,16 +198,39 @@ window.onload = function(){
         <p id="oppPhysical">${opponentList[radio2].physical} </p> 
         <p id="oppOverall">${wishlistOverall}</p>`;
 
-        /*var myPlayerOverall = Math.ceil((parseInt(playerList[radio1].pace) + parseInt(playerList[radio1].shooting) + parseInt(playerList[radio1].passing) + 
-        parseInt(playerList[radio1].dribbling) + parseInt(playerList[radio1].defence) + parseInt(playerList[radio1].physical)) / 6);
+        //function to reload page if there is missing data
+        function reloadPage(){
+            location.reload();
+            window.scrollTo(0,0);
+        }
 
-        console.log("my player overall = " + myPlayerOverall);
+        //checking for empty fields and displaying error
+        if (playerList[radio1].name === "" || playerList[radio1].age === "" || playerList[radio1].position === "X" || playerList[radio1].pace === "" 
+        || playerList[radio1].shooting === ""  || playerList[radio1].passing === ""  || playerList[radio1].dribbling === "" || playerList[radio1].defence === ""  
+        || playerList[radio1].physical  === "" )
+        {
+            compareContainer.style.display = "none";
+            error.style.display = "flex";
+            window.scrollBy(0,500);
+            errorMsg.innerHTML = `<p>Your Player's Data is incomplete. Please Verify player details.</p>`;
+            restart.onclick = reloadPage;
 
-        var wishlistOverall = Math.ceil((parseInt(opponentList[radio2].pace) + parseInt(opponentList[radio2].shooting) + parseInt(opponentList[radio2].passing) + 
-        parseInt(opponentList[radio2].dribbling) + parseInt(opponentList[radio2].defence) + parseInt(opponentList[radio2].physical)) / 6);
+        }else if (opponentList[radio2].name === "" || opponentList[radio2].age  === "" || opponentList[radio2].position  === "X" || opponentList[radio2].pace === ""
+        || opponentList[radio2].shooting === "" || opponentList[radio2].passing === "" || opponentList[radio2].dribbling === "" || opponentList[radio2].defence === "" 
+        || opponentList[radio2].physical === "")
+        {
+            compareContainer.style.display = "none";
+            error.style.display = "flex";
+            window.scrollBy(0,500);
+            errorMsg.innerHTML = `<p>Wishlist Player's Data is incomplete. Please Verify player details.</p>`;   
+            restart.onclick = reloadPage; 
+        
+        }else{
 
-        console.log("Wishlist overall = " + wishlistOverall);*/
+            //if no error, display the comparison
+        compareContainer.style.display = "flex"; //display the section to compare
 
+        //comapre my team player vs wishlsit player and display the better attribute in green and lesser attribute in red
         if (playerList[radio1].pace > opponentList[radio2].pace){
             document.getElementById("playerPace").style.backgroundColor = "#7CFC00";
             document.getElementById("oppPace").style.backgroundColor = "#FF0049";
@@ -287,15 +331,20 @@ window.onload = function(){
             document.getElementById("oppOverall").style.backgroundColor = "#FFFFFF";
         }
 
+        // creating the section to display name of better player
         var result = document.getElementById("result-display");
 
         if (myPlayerOverall > wishlistOverall){
             result.innerHTML = `<p> ${playerList[radio1].name} is the better player.</p>`;
 
-        }else{
+        }else if (myPlayerOverall < wishlistOverall){
             result.innerHTML = `<p> ${opponentList[radio2].name} is the better player. </p>`;
+        }else if(myPlayerOverall = wishlistOverall){
+            result.innerHTML = `<p> Both Players are Equal.</p>`;
+        }else{
+            betterPlayer.style.display = "none";//hide if error in data
         }
-        
+    } 
         
     } 
    
